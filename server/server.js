b6b4a3f4a9f0ac00fe01b031af26d70bf4362836
Sam Karp <sfcypret@gmail.com>
2022-10-30 15:41:21 -0400

    front end connection

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 // import apolloserver
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
+const path = require('path');
 
 //import our typedefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
@@ -27,6 +28,14 @@ const startApolloServer = async(typeDefs, resolvers) => {
   //integrate apollo server with express app as middleware
   server.applyMiddleware({ app });
 }
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
